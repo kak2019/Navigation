4844d59f811410d3d58c717045ebacb932a3131a
Flynt Gao <flynt.gao@consultant.udtrucks.com>
2023-05-11 13:54:54 +0800

    Change App.js file and implment click function

--- src/extensions/helloWorld/App.js
+++ src/extensions/helloWorld/App.js
@@ -1,16 +1,68 @@
-function loadstyle(){
-    //location.reload()
-    //var url  = window.location.href 
-    //var pagename = url.slice(url.lastIndexOf("/")+1,-5);
-    console.log("chufa");
-    document.getElementById("Delivery").className="unselectedItem";
-    document.getElementById("Integration").className="unselectedItem";
-    document.getElementById("UX").className="unselectedItem";
-    document.getElementById("AI").className="unselectedItem";
-    document.getElementById("RPA").className="unselectedItem";
-    //document.getElementById("RPA").className="unselectedItem";
-    document.getElementById("DocumentManagement").className="unselectedItem";
-    document.getElementById("LowcodeandMobile").className="unselectedItem";
-    document.getElementById("Community").className="unselectedItem";
+if (!window.Dep) {
+  class Dep {
+    constructor(name) {
+      this.id = new Date();
+      this.subs = [];
+    }
+    defined() {
+      Dep.watch.add(this);
+    }
+    notify() {
+      this.subs.forEach((e, i) => {
+        if (typeof e.update === 'function') {
+          try {
+            e.update.apply(e);
+          } catch (err) {}
+        }
+      });
+    }
   }
-  
+  Dep.watch = null;
+  window.Dep = Dep;
+}
+
+if (!window.Watch) {
+  class Watch {
+    constructor(name, fn) {
+      this.name = name;
+      this.id = new Date();
+      this.callBack = fn;
+    }
+    add(dep) {
+      dep.subs.push(this);
+    }
+    update() {
+      var cb = this.callBack;
+      cb(this.name);
+    }
+  }
+  window.Watch = Watch;
+
+  var addHistoryMethod = (function () {
+    var historyDep = new Dep();
+    return function (name) {
+      if (name === 'historychange') {
+        return function (name, fn) {
+          var event = new Watch(name, fn);
+          Dep.watch = event;
+          historyDep.defined();
+          Dep.watch = null; //置空供下一个订阅者使用
+        };
+      } else if (name === 'pushState' || name === 'replaceState') {
+        var method = history[name];
+        return function () {
+          method.apply(history, arguments);
+          historyDep.notify();
+        };
+      }
+    };
+  })();
+  window.addHistoryListener = addHistoryMethod('historychange');
+}
+
+history.pushState = addHistoryMethod('pushState');
+history.replaceState = addHistoryMethod('replaceState');
+
+function loadstyle() {
+  //EMPTY
+}
--- src/extensions/helloWorld/HelloWorldApplicationCustomizer.ts
+++ src/extensions/helloWorld/HelloWorldApplicationCustomizer.ts
@@ -1,10 +1,9 @@
 import { Log } from '@microsoft/sp-core-library';
 import { BaseApplicationCustomizer } from '@microsoft/sp-application-base';
-import { Dialog } from '@microsoft/sp-dialog';
-
 import * as strings from 'HelloWorldApplicationCustomizerStrings';
-import { Switch } from 'tdesign-react';
-//import {testinput} from './App.js'
+import './App.js';
+import './index.css';
+
 const LOG_SOURCE: string = 'HelloWorldApplicationCustomizer';
 
 /**
@@ -20,118 +19,100 @@ export interface IHelloWorldApplicationCustomizerProperties {
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class HelloWorldApplicationCustomizer extends BaseApplicationCustomizer<IHelloWorldApplicationCustomizerProperties> {
   public onInit(): Promise<void> {
+    console.log('INIT～～  ~');
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
     const head: any =
       document.getElementsByTagName('body')[0] || document.documentElement;
-    //const head1: any = document.getElementById('SuiteNavWrapper') || document.documentElement;
     const head1: any =
       document.getElementById('spSiteHeader') || document.documentElement;
-    // let message: string = this.properties.testMessage;
-    // if (!message) {
-    //   message = '(No properties were provided.)';
-    // }
 
-    // Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`).catch(() => {
-    //   /* handle error */
-    // });
+    // let articleRedirectScriptTag: HTMLScriptElement =
+    //   document.createElement('script');
+    // let jsurl = `${this.context.pageContext.web.serverRelativeUrl}/Shared%20Documents/App.js`;
+    // articleRedirectScriptTag.src = jsurl;
+    // articleRedirectScriptTag.type = 'text/javascript';
+    // head.insertAdjacentElement('beforeEnd', articleRedirectScriptTag);
 
-    let articleRedirectScriptTag: HTMLScriptElement =
-      document.createElement('script');
-    let jsurl = `${this.context.pageContext.web.serverRelativeUrl}/Shared%20Documents/App.js`;
-    articleRedirectScriptTag.src = jsurl;
-    articleRedirectScriptTag.type = 'text/javascript';
-    head.insertAdjacentElement('beforeEnd', articleRedirectScriptTag);
+    const root = document.getElementById('root');
 
-    //document.getElementById('SuiteNavWrapper').innerHTML='<div id="root" style = "display:flex"><div class="t-head-menu t-menu--dark"><div class="t-head-menu__inner"><ul class="t-menu"><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><hr style="display: none;"><span class ="spanfont">Home</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">Delivery Practice &amp; Framework</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">Integration &amp; Architecture</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">UX</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">AI</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">RPA</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">Document Management</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">Low code and Mobile</span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont">Community</span></span></li></ul></div></div></div>'
-    var url = window.location.href;
-    var pagename = url.slice(url.lastIndexOf('/') + 1, -5);
-
-    var selectedstyle;
     const innerHTML = `
-    <div id="root">
     <div class="t-head-menu t-menu--dark">
       <div class="t-head-menu__inner">
         <ul class="t-menu">
           <li class="t-menu__item t-menu__item--plain">
-            <span class="t-menu__content t-menu__content-active"><a class="t-menu__link" id="Home" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST">Home</a></span>
+           <a class="t-menu__link" id="Home" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST">Home</a>
           </li>
           <li class="t-menu__item t-menu__item--plain">
-            <span class="t-menu__content"><a class="t-menu__link" id="Delivery" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Delivery.aspx">Delivery Practice & Framework</a></span>
+           <a class="t-menu__link" id="Delivery" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Delivery.aspx">Delivery Practice & Framework</a>
           </li>
           <li class="t-menu__item t-menu__item--plain">
-            <span class="t-menu__content"><a class="t-menu__link" id="Integration" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Integration.aspx">Integration & Architecture</a></span>
+           <a class="t-menu__link" id="Integration" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Integration.aspx">Integration & Architecture</a>
           </li>
           <li class="t-menu__item t-menu__item--plain">
-            <span class="t-menu__content"> <a class="t-menu__link" id="UX" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/UX.aspx">UX</a></span>
+            <a class="t-menu__link" id="UX" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/UX.aspx">UX</a>
           </li>
           <li class="t-menu__item t-menu__item--plain">
-            <span class="t-menu__content"><a class="t-menu__link" id="AI" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/AI.aspx">AI</a></span>
+           <a class="t-menu__link" id="AI" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/AI.aspx">AI</a>
           </li>
-          <li class="t-menu__item t-menu__item--plain"><span class="t-menu__content">
-            <a class="t-menu__link" id="RPA" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/RPA.aspx">RPA</a></span>
+          <li class="t-menu__item t-menu__item--plain">
+           <a class="t-menu__link" id="RPA" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/RPA.aspx">RPA</a>
           </li>
-          <li class="t-menu__item t-menu__item--plain"><span class="t-menu__content">
-            <a class="t-menu__link" id="DocumentManagement" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/DocumentManagement.aspx">Document Management</a></span>
+          <li class="t-menu__item t-menu__item--plain">
+           <a class="t-menu__link" id="DocumentManagement" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/DocumentManagement.aspx">Document Management</a>
           </li>
 
-          <li class="t-menu__item t-menu__item--plain"><span class="t-menu__content">
-            <a class="t-menu__link" id="DocumentManagement" href="#">Low code and Mobile</a></span>
+          <li class="t-menu__item t-menu__item--plain">
+           <a class="t-menu__link" id="DocumentManagement" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/LowcodeandMobile.aspx">Low code and Mobile</a>
           </li> 
-          <li class="t-menu__item t-menu__item--plain"><span class="t-menu__content">
-            <a class="t-menu__link" id="DocumentManagement" href="#">Community</a></span>
+          <li class="t-menu__item t-menu__item--plain">
+           <a class="t-menu__link" id="DocumentManagement" href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Community.aspx">Community</a>
           </li> 
       </ul>
       </div>
-    </div>
     </div>`;
-    head1.insertAdjacentHTML('afterend', innerHTML);
 
-    //const innerHTML ='<div id="root" style = "display:flex; background-color:black"><div class="t-head-menu t-menu--dark"><div class="t-head-menu__inner"><ul class="t-menu"><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><hr style="display: none;"><span class ="spanfont" id="Home"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST">Home</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont" id="Delivery"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Delivery.aspx">Delivery Practice &amp; Framework</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont" id="Integration"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Integration.aspx">Integration &amp; Architecture</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/UX.aspx">UX</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/AI.aspx">AI</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/RPA.aspx">RPA</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/DocumentManagement.aspx">Document Management</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/LowcodeandMobile.aspx">Low code and Mobile</a></span></span></li><li class="t-menu__item t-menu__item--plain"><span class ="spanfont" class="t-menu__content"><span class ="spanfont"><a href="https://udtrucks.sharepoint.com/sites/ExtensionsTEST/SitePages/Community.aspx">Community</a></span></span></li></ul></div></div></div>'
-    //ead1.insertAdjacentHTML("afterend",innerHTML)
-    //require('./App.js');
-    let cssUrl = '/sites/ExtensionsTEST/Shared%20Documents/index.css';
-    let customStyle: HTMLLinkElement = document.createElement('link');
-    customStyle.href = cssUrl;
-    customStyle.rel = 'stylesheet';
-    customStyle.type = 'text/css';
-    head.insertAdjacentElement('beforeEnd', customStyle);
-    //addEventListener( 'load' , loadstyle);
+    if (root) {
+    } else {
+      head1.insertAdjacentHTML(
+        'afterend',
+        `
+        <div id="root">
+          ${innerHTML}
+        </div>
+      `
+      );
+
+      const checkActive = () => {
+        const pathname = window.location.href.split('?')[0];
+        const links = document.querySelectorAll('.t-menu__link');
+        links.forEach((link: any) => {
+          const isActive = pathname === link.href;
+          if (isActive) {
+            link.className = 't-menu__link t-menu__content-active';
+          } else {
+            link.className = 't-menu__link ';
+          }
+        });
+      };
+
+      checkActive();
+
+      (window as any).addHistoryListener('history', () => {
+        console.log('changed');
+        checkActive();
+      });
+      (window as any).addHistoryListener('popstate', () => {
+        console.log('goback');
+        checkActive();
+      });
+    }
+
+    // let cssUrl = '/sites/ExtensionsTEST/Shared%20Documents/index.css';
+    // let customStyle: HTMLLinkElement = document.createElement('link');
+    // customStyle.href = cssUrl;
+    // customStyle.rel = 'stylesheet';
+    // customStyle.type = 'text/css';
+    // head.insertAdjacentElement('beforeEnd', customStyle);
     return Promise.resolve();
   }
 }
-
-//  switch(pagename){
-//       case "Delivery":
-//         document.getElementById("Delivery").style.color="#1bb0d5";
-
-//       break;
-//       case "Integration":
-//         document.getElementById("Integration").style.color="#1bb0d5";
-
-//       break;
-//       case "UX":
-//         document.getElementById("UX").style.color="#1bb0d5";
-
-//       break;
-//       case "AI":
-//         document.getElementById("AI").style.color="#1bb0d5";
-
-//       break;
-//       case "RPA":
-//         document.getElementById("RPA").style.color="#1bb0d5";
-
-//       break;
-//       case "DocumentManagement":
-//         document.getElementById("DocumentManagement").style.color="#1bb0d5";
-
-//       break;
-//       case "LowcodeandMobile":
-//         document.getElementById("LowcodeandMobile").style.color="#1bb0d5";
-
-//       break;
-//       case "Community":
-//         document.getElementById("Community").style.color="#1bb0d5";
-
-//       break;
-//     }
-//     console.log("这玩意运行了吗")
